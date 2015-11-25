e932a5e6240a25ed9f68c4b88f875111cc5b5066
MarkWaldron <markewaldron@gmail.com>
2015-11-25 12:36:49 -0500

    Added ability to append multiple days to itinerary

--- public/js/liveUI.js
+++ public/js/liveUI.js
@@ -2,7 +2,7 @@
   if($('.hotelList').children().length < 1){
     var hotelName = $('#hotels').find(':selected').text();
     $('.hotelList')
-    .append("<div class='itinerary-item'><span class='title'>" + hotelName + "</span><button class='btn btn-xs btn-danger remove btn-circle hotelRemove'>x</button></div>");
+    .append("<div class='itinerary-item'><span class='title'>" + hotelName + "</span><button class='btn btn-xs btn-danger remove btn-circle'>x</button></div>");
   }
  });
 
@@ -10,7 +10,7 @@
    if($('.restaurantList').children().length < 3){
      var restaurantName = $('#restaurants').find(':selected').text();
      $('.restaurantList')
-     .append("<div class='itinerary-item'><span class='title'>" + restaurantName + "</span><button class='btn btn-xs btn-danger remove btn-circle restaurantRemove'>x</button></div>");
+     .append("<div class='itinerary-item'><span class='title'>" + restaurantName + "</span><button class='btn btn-xs btn-danger remove btn-circle'>x</button></div>");
    }
  });
 
@@ -18,6 +18,27 @@
    if($('.activityList').children().length < 3){
      var activityName = $('#activities').find(':selected').text();
      $('.activityList')
-     .append("<div class='itinerary-item'><span class='title'>" + activityName + "</span><button class='btn btn-xs btn-danger remove btn-circle activityRemove'>x</button></div>");
+     .append("<div class='itinerary-item'><span class='title'>" + activityName + "</span><button class='btn btn-xs btn-danger remove btn-circle'>x</button></div>");
    }
  });
+
+ $('.hotelList').on('click', '.btn', function(){
+   $(this).parent().remove();
+ })
+
+ $('.restaurantList').on('click', '.btn', function(){
+   $(this).parent().remove();
+ })
+
+ $('.activityList').on('click', '.btn', function(){
+   $(this).parent().remove();
+ })
+
+ $('.day-buttons').on('click', '.addDay', function(){
+   var numberOfDays = $('.day-buttons').children().length;
+   $('.current-day').removeClass('current-day');
+
+   $('.day-buttons').append("<button class='btn btn-circle day-btn current-day'>" + numberOfDays + "</button>");
+   $('.addDay').remove();
+   $('.day-buttons').append("<button class='btn btn-circle day-btn addDay'>+</button>")
+ })
